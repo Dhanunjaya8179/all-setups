866888f3330bea1ee4301b76de88b1d0fea493c0
Dhanunjaya8179 <95037375+Dhanunjaya8179@users.noreply.github.com>
2025-08-09 14:30:31 +0530

    Update linux-jenkins.sh

--- linux-jenkins.sh
+++ linux-jenkins.sh
@@ -5,3 +5,13 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     sudo yum install jenkins -y
     sudo systemctl enable jenkins
     sudo systemctl start jenkins
+
+
+
+    sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
